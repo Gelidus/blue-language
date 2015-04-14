6c0c75795c42d0917f19626545b2903927ce6a6e
Gelidus <gelidus@hotmail.sk>
2015-04-14 11:40:33 +0200

    Expression progress

--- generated/property.cpp
+++ generated/property.cpp
@@ -5,3 +5,15 @@ int func (int a, int b) {
   return a + b;
 }
 
+float ffunc (float a, float b) {
+  return a - b;
+}
+
+int main () {
+  int a = 5;
+  int b = 6;
+  print(a)print(b);
+  int sum = a + b;
+  print(sum);
+}
+
